4b4b0df7e8fadfa06fa485537ca96063c2342334
HyeseonGwon <hyeseon.gwon@sk.com>
2026-07-03 17:15:10 +0900

    10일차

---
+++ 10일차/_list_cells.py
@@ -0,0 +1,6 @@
+﻿import json
+with open(r"c:\Users\Admin\Desktop\실습\10일차\머신러닝 Day 3. 미니프로젝트.ipynb", encoding="utf-8") as f:
+    nb = json.load(f)
+for i, c in enumerate(nb["cells"]):
+    src = "".join(c.get("source", []))[:80].replace("\n", " ")
+    print(i, c["cell_type"], repr(src))
